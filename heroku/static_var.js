98c57e64601cee6dd88e8fbaea714b31d3fc9c96
Simone Ferretti <simone.ferrettii91@gmail.com>
2017-03-17 15:57:06 +0100

    images URLs updated

--- heroku/static_var.js
+++ heroku/static_var.js
@@ -10,12 +10,11 @@ var hellolist = ['ciao', 'getstarted', 'start', 'cominciamo', 'iniziamo', 'inizi
 var hellomessage = "! Sono HiFretta! Inviami la tua posizione per sapere tra quanto passa l\'auto che ti serve nelle fermate vicine!\nPer sapere in che altro modo ti posso aiutare digita aiuto."
 
 //URL of images
-var logo_atac_URL = "https://bytebucket.org/simoneferretti/hifretta_botfaceboook/raw/dd1d246bcf006b41e26e420aa44e5ac11ebcec71/images/Logo_ATAC.png?token=8b1ebd24c5b19c98f526a4b48c449b079ccaf217";
-var posizione_gif_URL = "https://bytebucket.org/simoneferretti/hifretta_botfaceboook/raw/dd1d246bcf006b41e26e420aa44e5ac11ebcec71/images/posizione.gif?token=c85f2ced0c1289c88633489b84d2705458117283";
-var id_palina_URL = "https://bytebucket.org/simoneferretti/hifretta_botfaceboook/raw/dd1d246bcf006b41e26e420aa44e5ac11ebcec71/images/id_palina.jpg?token=ef009e30a66af24d267b4a2d3fee870e0fe1be15";
-var bus_example_URL = "https://bytebucket.org/simoneferretti/hifretta_botfaceboook/raw/dd1d246bcf006b41e26e420aa44e5ac11ebcec71/images/bus_example.jpg?token=66b170d407b21815a9a2f8656abc7787b21dbc1c";
-var cat_gif_URL = "https://bytebucket.org/simoneferretti/hifretta_botfaceboook/raw/dd1d246bcf006b41e26e420aa44e5ac11ebcec71/images/gif-cat.gif?token=1c34e077774512e781ce18173f1342ba43238fde";
-
+var logo_atac_URL = "https://github.com/rfidlabsapienza/Hifretta/blob/master/images/Logo_ATAC.png?raw=true";
+var posizione_gif_URL = "https://github.com/rfidlabsapienza/Hifretta/blob/master/images/posizione.gif";
+var id_palina_URL = "https://github.com/rfidlabsapienza/Hifretta/blob/master/images/id_palina.jpg?raw=true";
+var bus_example_URL = "https://github.com/rfidlabsapienza/Hifretta/blob/master/images/bus_example.jpg?raw=true";
+var cat_gif_URL = "https://github.com/rfidlabsapienza/Hifretta/blob/master/images/gif-cat.gif?raw=true";
 module.exports.DEV_KEY = DEV_KEY;
 module.exports.helplist = helplist;
 module.exports.benvenuto = benvenuto;
